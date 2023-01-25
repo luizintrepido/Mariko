2ec6f2b7328391a4e5fe0da14862f9557387bf82
Luiz Intrépido <luizintrepido@gmail.com>
2023-01-25 00:09:34 -0300

    1

---
+++ swiper.js
@@ -0,0 +1,30 @@
+var swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: false,
+  mousewheel: true,
+  
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    
+    
+    
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+
+  
+});
+
